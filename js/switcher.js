6816ee48811e6da48391918b3ab5f7ca7c93f5a1
danya <lovec_omarov@mail.ru>
2023-05-29 13:51:22 +0300

    Refactored files

--- js/switcher.js
+++ js/switcher.js
@@ -7,4 +7,17 @@ btn.onclick = (e) => {
     } else {
         theme.href = 'css/light-theme.css';
     }
+    let CatalogSection = document.querySelector('.section-catalog');
+    if (CatalogSection.style.backgroundColor === '') {
+        CatalogSection.style.backgroundColor = '#414141';
+    } else {
+        CatalogSection.style.backgroundColor = '';
+    }
+
+    let ContactsSection = document.querySelector('.section-contacts');
+    if (ContactsSection.style.backgroundColor === '') {
+        ContactsSection.style.backgroundColor = '#414141';
+    } else {
+        ContactsSection.style.backgroundColor = '';
+    }
 }
